d144e5f2c599772addb1baacbd42c9d24d3ebd19
PrajwalCH <prajjwal2058@gmail.com>
2021-06-29 13:38:43 +0545

    add code for testing new api

--- test/test.c
+++ test/test.c
@@ -5,12 +5,26 @@ extern StringOptions default_string_option;
 
 int main(void)
 {
+  printf("____char\n");
   char single_char = 'a';
   printf("char add: %p\n", &single_char);
   readtrmin_char(&single_char);
-  printf("%c", single_char);
+  printf("%c\n", single_char);
 
-  //char string[5];
-  //readtrmin_string(string, 5, 4, &default_string_option);
-  //printf("%s\n", string);
+  printf("____string\n");
+  StringOptions user_option = {
+    .allow_space = false,
+    .allow_number = false,
+    .allow_symbol = true,
+    .allow_uppercase = false,
+    .allow_lowercase = true
+  };
+  char string[5];
+  readtrmin_string(string, 5, 3, &user_option);
+  printf("%s\n", string);
+  
+  printf("____int\n");
+  long lg_int = 0;
+  readtrmin_int(&lg_int, 1);
+  printf("%li\n", lg_int);
 }
