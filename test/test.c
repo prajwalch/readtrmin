c6429c0f59ae8e5bdbdde27211dd04ec8ae18e2a
PrajwalCH <prajjwal2058@gmail.com>
2021-06-29 19:05:28 +0545

    some new code for testing

--- test/test.c
+++ test/test.c
@@ -6,14 +6,13 @@ extern StringOptions default_string_option;
 int main(void)
 {
   printf("____char\n");
-  char single_char = 'a';
-  printf("char add: %p\n", &single_char);
+  char single_char = 0;
   readtrmin_char(&single_char);
   printf("%c\n", single_char);
 
   printf("____string\n");
   StringOptions user_option = {
-    .allow_space = false,
+    .allow_space = true,
     .allow_number = false,
     .allow_symbol = true,
     .allow_uppercase = false,
@@ -25,6 +24,6 @@ int main(void)
   
   printf("____int\n");
   long lg_int = 0;
-  readtrmin_int(&lg_int, 1);
+  readtrmin_int(&lg_int, 9);
   printf("%li\n", lg_int);
 }
